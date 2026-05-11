d8285d3a6bb53127af05cd0dd2aa359c8acd4304
Joe <joe.hunter.dev@gmail.com>
2026-05-11 19:31:01 +0200

    deploy test

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
-    'version' => env('APP_VERSION', '1.6.4'),
+    'version' => env('APP_VERSION', '1.6.6'),
 
     /*
     |--------------------------------------------------------------------------
