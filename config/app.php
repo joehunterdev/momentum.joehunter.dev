27d0642c16335c1e6b437aa46f023c9ff10b7171
Joe <joe.hunter.dev@gmail.com>
2026-05-13 18:20:50 +0200

    run test

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
-    'version' => env('APP_VERSION', '1.6.9'),
+    'version' => env('APP_VERSION', '1.7.0'),
 
     /*
     |--------------------------------------------------------------------------
