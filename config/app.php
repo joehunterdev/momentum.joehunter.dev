6714ca61467dbdb9d56c8aa3ad6b6a8828bc8c3b
Joe <joe.hunter.dev@gmail.com>
2026-05-14 19:21:14 +0200

    testing

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Momentum'),
 
-    'version' => env('APP_VERSION', '1.7.2'),
+    'version' => env('APP_VERSION', '1.7.4'),
 
     /*
     |--------------------------------------------------------------------------
