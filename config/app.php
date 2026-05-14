eec595bb7f99c2feeec0e2b77b5c6550dc58993d
Joe <joe.hunter.dev@gmail.com>
2026-05-14 19:16:19 +0200

    chore: update deployment workflow and application version to 1.7.2

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Momentum'),
 
-    'version' => env('APP_VERSION', '1.7.1'),
+    'version' => env('APP_VERSION', '1.7.2'),
 
     /*
     |--------------------------------------------------------------------------
