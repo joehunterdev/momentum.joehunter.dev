b355aa3dd43329d308f92647f3b17c2b0899b6d8
Joe <joe.hunter.dev@gmail.com>
2026-05-21 22:12:54 +0200

    built

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Momentum'),
 
-    'version' => env('APP_VERSION', '1.8.0'),
+    'version' => env('APP_VERSION', '1.8.5'),
 
     /*
     |--------------------------------------------------------------------------
