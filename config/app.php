39a303e0a5b728525205048fd93aa5fd098797c5
Joe <joe.hunter.dev@gmail.com>
2026-05-21 21:20:20 +0200

    deploy 1.8.0

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Momentum'),
 
-    'version' => env('APP_VERSION', '1.7.9'),
+    'version' => env('APP_VERSION', '1.8.0'),
 
     /*
     |--------------------------------------------------------------------------
