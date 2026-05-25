54f03ac4ad14e2c882353a241460141b3903cf15
Joe <joe.hunter.dev@gmail.com>
2026-05-25 02:31:23 +0200

    app versoin

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Momentum'),
 
-    'version' => env('APP_VERSION', '2.0.0'),
+    'version' => env('APP_VERSION', '2.1.0'),
 
     /*
     |--------------------------------------------------------------------------
