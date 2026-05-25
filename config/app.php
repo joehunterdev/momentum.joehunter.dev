d31d1e0b9c35be5103dfff83ba5bf0fa3bcf450d
Joe <joe.hunter.dev@gmail.com>
2026-05-25 05:52:21 +0200

    deploy test

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Momentum'),
 
-    'version' => env('APP_VERSION', '2.2.0'),
+    'version' => env('APP_VERSION', '2.2.2'),
 
     /*
     |--------------------------------------------------------------------------
